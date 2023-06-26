72d8719b2ccbb92adfbe0517b33779d01b849129
Aurélien Cros <aure.cros@gmail.com>
2023-06-26 17:05:44 +0200

    main/init
    Initial commit with webpack configuration and the start of static integration

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/main.scss';
+import './assets/fonts/Orbitron/Orbitron-VariableFont_wght.ttf';
+import './assets/fonts/Sansation/Sansation_Regular.ttf';
+import './assets/fonts/Sansation/Sansation_Bold.ttf';
+
+import './assets/icons/weather-partly-cloudy.svg';
+import './assets/icons/details-humidity.svg';
