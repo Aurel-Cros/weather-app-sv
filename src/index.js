e26328a4e358ea4720ef54fad8710d0f0b51d55b
Aurélien Cros <aure.cros@gmail.com>
2023-06-27 11:22:14 +0200

    feat/main : themes
    Added weather themes backgrounds

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import './styles/main.scss';
+import './styles/themes.scss';
+
 import './assets/fonts/Orbitron/Orbitron-VariableFont_wght.ttf';
 import './assets/fonts/Sansation/Sansation_Regular.ttf';
 import './assets/fonts/Sansation/Sansation_Bold.ttf';
 
+import './assets/icons/weather-cloudy.svg';
+import './assets/icons/weather-fog.svg';
+import './assets/icons/weather-hail.svg';
+import './assets/icons/weather-sunny.svg';
 import './assets/icons/weather-partly-cloudy.svg';
-import './assets/icons/details-humidity.svg';
+import './assets/icons/weather-rain.svg';
+import './assets/icons/weather-snow.svg';
+import './assets/icons/weather-sunny.svg';
+import './assets/icons/weather-thunderstorm.svg';
+
+import './assets/icons/lock-closed.svg';
+import './assets/icons/lock-open.svg';
+
+import './assets/icons/search.svg';
+import './assets/icons/swipe-left.svg';
 
 import './styles/clock.scss';
 import './components/clock';
