c7b9c7d176c10d1b99913291d067b77849007c1f
Aurélien Cros <aure.cros@gmail.com>
2023-06-28 09:22:12 +0200

    refactor/main : weather wheel
    Converted the weather wheel component to a class

--- src/components/app.js
+++ src/components/app.js
@@ -1,1 +1,5 @@
+import WeatherWheel from './weather-wheel';
+const button = document.querySelector(".current-weather img");
+button.addEventListener("click", () => { new WeatherWheel() });
+
 // On open, fetch information of random cities
--- src/components/weather-wheel.js
+++ src/components/weather-wheel.js
@@ -1,39 +1,46 @@
-const $wrapper = document.querySelector(".current-weather");
-const button = document.querySelector(".current-weather img");
-button.addEventListener("click", openWheel);
+export default class WeatherWheel {
+    constructor() {
+        this.$wrapper = document.querySelector(".current-weather");
+        this.openWheel();
+    }
+    closeWheel() {
+        this.wheelHub.remove();
+    }
+    openWheel() {
+        console.log('Open the wheel !');
+        // create wheel element
+        this.wheelHub = document.createElement("div");
+        this.wheelHub.className = "wheel-hub";
 
-function openWheel() {
-    console.log('Open the wheel !');
-    // create wheel element
-    const wheelHub = document.createElement("div");
-    wheelHub.className = "wheel-hub";
+        const weatherList = [
+            'sunny',
+            'partly-cloudy',
+            'cloudy',
+            'rain',
+            'thunderstorm',
+            'snow',
+            'fog',
+            'hail'
+        ]
 
-    const weatherList = [
-        'sunny',
-        'partly-cloudy',
-        'cloudy',
-        'rain',
-        'thunderstorm',
-        'snow',
-        'fog',
-        'hail'
-    ]
+        weatherList.forEach((weather, index) => {
+            const element = document.createElement("div");
+            element.className = "wheel-element";
+            element.style.rotate = `${index * 45}deg`;
 
-    weatherList.forEach((weather, index) => {
-        const element = document.createElement("div");
-        element.className = "wheel-element";
-        element.style.rotate = `${index * 45}deg`;
+            const bgHolder = document.createElement("button");
+            bgHolder.className = "wheel-element-background";
+            bgHolder.style.backgroundImage = `url('weather-${weather}.svg`;
+            bgHolder.style.rotate = `-${index * 45}deg`;
 
-        const bgHolder = document.createElement("div");
-        bgHolder.className = "wheel-element-background";
-        bgHolder.style.backgroundImage = `url('weather-${weather}.svg`;
-        bgHolder.style.rotate = `-${index * 45}deg`;
+            bgHolder.addEventListener("click", () => this.closeWheel());
+            element.appendChild(bgHolder);
+            this.wheelHub.appendChild(element);
+        })
+        const divMask = document.createElement("div");
+        divMask.className = "wheel-hub-background";
+        this.wheelHub.appendChild(divMask);
 
-        element.appendChild(bgHolder);
-        wheelHub.appendChild(element);
-    })
-    const divMask = document.createElement("div");
-    divMask.className = "wheel-hub-background";
-    wheelHub.appendChild(divMask);
-    $wrapper.appendChild(wheelHub);
+        this.$wrapper.appendChild(this.wheelHub);
+    }
 }
