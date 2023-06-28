f0fcc5e0449ef29204c9b170a99e8c17b482fcd9
Aurélien Cros <aure.cros@gmail.com>
2023-06-28 10:47:33 +0200

    feat/main : date
    Display today date on open

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,28 @@
 import WeatherWheel from './WeatherWheel';
 import Clock from './Clock';
 
-const button = document.querySelector(".current-weather img");
-button.addEventListener("click", () => { new WeatherWheel() });
+const currWeatherIcon = document.querySelector(".current-weather img");
+currWeatherIcon.addEventListener("click", () => { new WeatherWheel() });
 
 const clockElement = new Clock('.weather-graph-values');
+
+// Select data elements
+const $ = {
+    currentWeather: document.querySelector(".current-weather p"),
+    cityName: document.querySelector(".city-name"),
+    countryName: document.querySelector(".country"),
+    countyName: document.querySelector(".country span"),
+    highTemp: document.querySelector(".temp-hi"),
+    lowTemp: document.querySelector(".temp-lo"),
+    mainTemp: document.querySelector(".main-temp"),
+    mainWind: document.querySelector(".weather-graph-values .wind"),
+    mainHumi: document.querySelector(".weather-graph-values .humidity"),
+    todayDate: document.querySelector(".today-date"),
+    fiveDays: document.querySelector(".five-days-pred"),
+    shortText: document.querySelector(".short-text")
+}
+
+const today = new Date();
+$.todayDate.textContent = `${today.toLocaleString('en-en', { weekday: 'short' })} ${today.getMonth()}/${today.getDate()}`;
 
 // On open, fetch information of random cities
